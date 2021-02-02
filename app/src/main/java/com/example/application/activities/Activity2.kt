d921eb86b8316119329de42993489114e72a51d3
iseletkov <iseletkov@gmail.com>
2021-02-02 17:33:23 +0500

    Пример передачи данных в дочернюю форму и обратно.

---
+++ app/src/main/java/com/example/application/activities/Activity2.kt
@@ -0,0 +1,34 @@
+package com.example.application.activities
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.example.application.R
+
+class Activity2 : AppCompatActivity() {
+    private lateinit var et1     : EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+
+        et1        = findViewById(R.id.et1)
+
+        val message = intent.getStringExtra("Параметр")
+        et1.setText(message)
+    }
+    fun onButtonInputPress(view: View)
+    {
+        val text = et1.text
+
+        val intent = Intent("123").apply {
+            putExtra("Параметр2", text.toString())
+        }
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+}
